6f3e8874658fa9b37632fb795332f8ce1574d4c8
Lim Shi Jie <shijielim@gmail.com>
2023-08-15 16:18:51 +0800

    done

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/";
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  result.textContent = "loading..."; //can give loading first cause not promise will just load
+  try {
+    const resp = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!resp.ok) {
+      throw new Error("there was an error");
+    }
+    // console.log(resp); response object
+    const data = await resp.json();
+    // console.log(data); data object
+    result.textContent = data.joke;
+  } catch (err) {
+    result.textContent = "There was an error..";
+  }
+};
+
+fetchData();
